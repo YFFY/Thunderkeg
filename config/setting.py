9a43b0ac890934f5fffb5c04923295bacf1c3b5f
yfsuse <yfsuse@gmail.com>
2015-05-14 11:03:53 +0800

    update util module change setting

--- config/setting.py
+++ config/setting.py
@@ -4,9 +4,6 @@
 # running configure, when import druid data with this tool, this file should be configure carefully at the beginning
 
 indexFileList = ["native.indexing.json"]
-# importDataUrl = "http://10.1.5.61:8090/druid/indexer/v1/task"   # post this
-# queryStatusUrl = "http://10.1.5.61:8090/druid/indexer/v1/task/{0}/status" # get this
-
-importDataUrl = "http://172.20.0.49:8090/druid/indexer/v1/task"   # post this
-queryStatusUrl = "http://172.20.0.49:8090/druid/indexer/v1/task/{0}/status" # get this
+importDataUrl = "http://10.1.5.61:8090/druid/indexer/v1/task"   # post this
+queryStatusUrl = "http://10.1.5.61:8090/druid/indexer/v1/task/{0}/status" # get this
 wait_time = 1800
